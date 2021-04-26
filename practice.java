87c633b90f09ee3c35986a88c297e0bf50e947d7
prudhvi <prudhvieethakota123@gmail.com>
2021-04-26 06:21:21 +0530

    Added:Form the Right angle Triangle

--- practice.java
+++ practice.java
@@ -2,5 +2,11 @@ class practice {
 
    public static void main(String[] args) {
            System.out.println("* Triangle in java");
+      for (int i = 1; i <= 5; i++ ) {
+         for (int j = 1; j <= i; j++ ) {
+         System.out.print("* ");
+ 	 }
+      System.out.println();
+      }
    }
 }
