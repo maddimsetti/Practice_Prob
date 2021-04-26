b2f047968bcbf02dc8c3bbcdfdebe432879e841c
maddimsetti <82135298+maddimsetti@users.noreply.github.com>
2021-04-26 06:37:03 +0530

    Form the reversed Right Triangle

--- practice.java
+++ practice.java
@@ -2,7 +2,7 @@ class practice {
 
    public static void main(String[] args) {
            System.out.println("* Triangle in java");
-      for (int i = 1; i <= 5; i++ ) {
+      for (int i = 5; i <= 1; i-- ) {
          for (int j = 1; j <= i; j++ ) {
          System.out.print("* ");
  	 }
